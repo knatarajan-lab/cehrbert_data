8c2427705d8427f66bd6e633e4035c3c88edb207
Chao Pang <ChaoPang229@gmail.com>
2024-11-03 08:55:46 -0500

    update the cohort name in the args

--- src/cehrbert_data/tools/prepare_ehrshot_cohorts.py
+++ src/cehrbert_data/tools/prepare_ehrshot_cohorts.py
@@ -7,11 +7,15 @@ if __name__ == "__main__":
     cohort_dir = args.cohort_dir
     output_folder = args.output_folder
     for individual_cohort in os.listdir(cohort_dir):
+        if individual_cohort.endswith('/'):
+            individual_cohort = individual_cohort[:-1]
+        cohort_name = os.path.basename(individual_cohort)
         args_copy = copy.copy(args)
         individual_cohort_dir = os.path.join(cohort_dir, individual_cohort)
         individual_output_folder = os.path.join(output_folder, individual_cohort)
         args_copy.cohort_dir = individual_cohort_dir
         args_copy.output_folder = individual_output_folder
+        args_copy.cohort_name = cohort_name
         main(
             args_copy
         )
