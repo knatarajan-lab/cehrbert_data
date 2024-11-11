6630488d33b975b942a6b379730142c9480dbcde
Chao Pang <ChaoPang229@gmail.com>
2024-11-11 13:25:45 -0500

    Revert "removed event_group_ids from the cehrgpt input data"
    This reverts commit 38742ec850f0ec6561bd8a807be7532ed89d1688.

--- src/cehrbert_data/utils/spark_utils.py
+++ src/cehrbert_data/utils/spark_utils.py
@@ -765,7 +765,8 @@ def create_sequence_data_with_att(
         "visit_rank_orders",
         "concept_orders",
         "record_ranks",
-        "units"
+        "units",
+        "event_group_ids",
     ]
 
     patient_grouped_events = (
@@ -796,6 +797,7 @@ def create_sequence_data_with_att(
         .withColumn("mlm_skip_values", F.col("data_for_sorting.mlm_skip_value"))
         .withColumn("visit_concept_ids", F.col("data_for_sorting.visit_concept_id"))
         .withColumn("units", F.col("data_for_sorting.unit"))
+        .withColumn("event_group_ids", F.col("data_for_sorting.event_group_id"))
     )
 
     return patient_grouped_events.select(output_columns)
