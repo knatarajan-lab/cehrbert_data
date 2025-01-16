4b94b701da83ea282c80c220d893c4ff91300d16
Chao Pang <ChaoPang229@gmail.com>
2025-01-16 09:41:00 -0500

    try to fix the java incompatibility issue with pyspark

--- tests/pyspark_test_base.py
+++ tests/pyspark_test_base.py
@@ -22,7 +22,17 @@ class PySparkAbstract(unittest.TestCase, ABC):
     def setUp(self):
         from pyspark.sql import SparkSession
 
+        # The error InaccessibleObjectException: Unable to make private java.nio.DirectByteBuffer(long,int) accessible
+        # occurs because the PySpark code is trying to make a private constructor accessible, which is prohibited by
+        # the Java module system for security reasons.
+        # Starting from Java 9, JPMS enforces strong encapsulation of Java modules unless explicitly opened up.
+        # This means that unless the java.base module explicitly opens the java.nio package to your application,
+        # reflection on its classes and members will be blocked.
+        # Add JVM Options: If you must use a newer version of Java, you can try adding JVM options to open up the
+        # necessary modules. This is done by adding arguments to the spark.driver.extraJavaOptions and
+        # spark.executor.extraJavaOptions in your Spark configuration:
         self.spark = SparkSession.builder.master("local").appName("test").getOrCreate()
+
         # Get the root folder of the project
         root_folder = Path(os.path.abspath(__file__)).parent.parent
         self.data_folder = os.path.join(root_folder, "sample_data", "omop_sample")
